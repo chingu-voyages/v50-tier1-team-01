02c167215f19f512eb67866039798e2be0fb8d82
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-15 02:51:50 +0200

    Retrieve menu dataset from menu API

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector("#menu")
+
+async function getMenuData() {
+    const url = "https://menus-api.vercel.app";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      return json;
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+const menuData = getMenuData();
