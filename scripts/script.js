5d2c63a23684d83365265876145c4910c05c8074
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-07-17 17:59:39 +0100

    first pass of validation to form

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
-// Login modal
+// login modal
 
 document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('loginModal');
   const btn = document.getElementById('openModalBtn');
   const span = document.getElementsByClassName('close')[0];
   const form = document.getElementById('loginForm');
+  let username = document.getElementById('username');
+  let password = document.getElementById('password');
 
+  //modal display
   btn.onclick = function () {
     modal.style.display = 'block';
   };
@@ -20,12 +23,58 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  //form submit
   form.onsubmit = function (event) {
     event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
     modal.style.display = 'none';
     console.log('username:', username);
     console.log('password:', password);
+
+    validateInputs();
+  };
+
+  const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+  };
+
+  const setSuccess = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+  };
+
+  // const isValidEmail = (email) => {
+  //   const re =
+  //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //   return re.test(String(email).toLowerCase());
+  // };
+
+  const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue === '') {
+      setError(username, 'Username is required');
+    } else {
+      setSuccess(username);
+    }
+
+    if (passwordValue === '') {
+      setError(password, 'Password is required');
+    } else if (passwordValue.length < 6) {
+      setError(password, 'Password must be at least 6 characters!');
+    } else {
+      setSuccess(password);
+    }
   };
 });
