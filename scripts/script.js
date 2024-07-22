882b60f04a1aaf7f3b133cc5fc5b88c7a0668779
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-22 23:26:47 +0200

    Display menu items

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const menu_div = document.querySelector("#menu-div");
+const menu_list = document.querySelector("#menu-list");
 
 async function getMenuData() {
     const url = "https://menus-api.vercel.app";
@@ -8,8 +8,38 @@ async function getMenuData() {
         throw new Error(`Response status: ${response.status}`);
       }
   
+      // Select only the pizza from the returned menu data
       const json = await response.json();
-      console.log(json);
+      const pizzas_all = json.pizzas; // select only pizzas
+      
+      // Select the first 20 pizza items
+      const pizzas_selected = pizzas_all.slice(0, 20);
+
+      // Create tile for each pizza
+      for (let i = 0; i < 20; i++) {
+        menu_list.innerHTML += `
+          <li>
+            <div class="menu-item">
+              <img class="menu-item-image" src=${pizzas_selected[i].img}>
+
+              <div class="menu-item-details-row-1">
+                <div class="menu-item-name"> ${pizzas_selected[i].dsc} </div>
+                <div class="menu-item-price"> $${pizzas_selected[i].price} </div>
+              </div>
+
+              <div class="menu-item-details-row-2">
+                <div class="menu-item-rating"> ${pizzas_selected[i].rate} </div>
+                <button class="add-to-basket-button"></button>
+              </div>
+
+            </div>
+          </li>
+        `
+      }
+      
+
+      console.log(pizzas_selected); 
+
 
     } catch (error) {
       console.error(error.message);
